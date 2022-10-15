495a02460898a022c507ad03ff561433210e9ef2
Shrinath Thube <shrinaththube@gmail.com>
2022-10-15 01:40:37 -0700

    hw: hello-world in java

---
+++ hello-world/helloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * run command
+ * First compile java code -> javac helloWorld.java
+ * Run compiled java class -> java helloWorld
+ * If you change anything in the code, repeat first and second step to test your logic.
+ */
+class helloWorld{
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World!");   
+    
+    }
+
+}
