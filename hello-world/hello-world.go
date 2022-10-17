535a860b33c107bc1efecddeed553ae8b8efe35e
Shrinath Thube <shrinaththube@gmail.com>
2022-10-16 21:48:18 -0700

    hw: golang

---
+++ hello-world/hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+// run command ref - https://gobyexample.com/hello-world
+// only run - go run hello-world.go
+// build and run - go build hello-world.go
+// ./hello-world
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!")
+
+}
