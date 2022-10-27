678dacb5acc438a587fb3f1d59c7401eec79dfd4
Shrinath Thube <shrinaththube@gmail.com>
2022-10-26 22:49:27 -0700

    read: python

---
+++ read/python-read.py
@@ -0,0 +1,21 @@
+
+
+# https://www.freecodecamp.org/news/how-to-read-files-in-python/
+# https://www.pythontutorial.net/python-basics/python-read-text-file/
+
+filename = "sample-read.txt"
+
+print("*"*40)
+print("1) Read line by line")
+print("*"*40)
+with open(filename) as f:
+    for line in f.readlines():
+        print(line)
+
+print("*"*40)
+print("2) Read entire file")
+print("*"*40)
+
+file_operator = open(filename,'r')
+print(file_operator.read())
+file_operator.close()
